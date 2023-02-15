d0132e72b0b07acd6b959d702dd74a2a59640e66
M-Orxan <orxanm385@gmail.com>
2023-02-15 16:21:21 +0400

    FixingErrors

--- Number Guessing Game/Number Guessing Game/MainWindow.xaml.cs
+++ Number Guessing Game/Number Guessing Game/MainWindow.xaml.cs
@@ -29,7 +29,10 @@ namespace Number_Guessing_Game
         int maxValue;
         int randomNumber;
         int userNumber;
-        int count = 5;
+       
+        int chancesCount=0;
+
+            
 
         private void SetTheRange_Click(object sender, RoutedEventArgs e)
         {
@@ -37,6 +40,14 @@ namespace Number_Guessing_Game
 
             string minimumValue = MinimumValueInput.Text;
             string maximumValue = MaximumValueInput.Text;
+
+            if (!int.TryParse(maximumValue, out maxValue)&& !int.TryParse(minimumValue, out minValue))
+            {
+                MessageBox.Show("You entered invalid values.\nPlease try again");
+                MaximumValueInput.Clear();
+                MinimumValueInput.Clear();
+                return;
+            }
 
 
             if (string.IsNullOrEmpty(MinimumValueInput.Text))
@@ -67,17 +78,38 @@ namespace Number_Guessing_Game
                 return;
             }
 
+            if (string.IsNullOrEmpty(ChancesInput.Text))
+            {
+                MessageBox.Show("You didn't enter chances count.");
+
+                return;
+            }
+
+            if (!int.TryParse(ChancesInput.Text, out chancesCount))
+            {
+                MessageBox.Show("You entered invalid chances count");
+                ChancesInput.Clear();
+                return;
+            }
+
+            MessageBox.Show($"You have {chancesCount} chances.\nYou should find the number between {minimumValue} and {maximumValue} (both includes)");
+
             Random random = new Random();
 
             randomNumber = random.Next(minValue, maxValue + 1);
 
             label.Content = $"Now please enter number between {minValue} and {maxValue}";
+
+           
+            
+
         }
 
 
 
         private void Check_Click(object sender, RoutedEventArgs e)
         {
+
             if (string.IsNullOrEmpty(UsernumberInput.Text))
             {
                 MessageBox.Show("You didn't enter the number");
@@ -92,14 +124,15 @@ namespace Number_Guessing_Game
                 return;
             }
 
-            if (count == 0 && userNumber != randomNumber)
+            
+            if (chancesCount == 1 && userNumber != randomNumber)
             {
                 MessageBox.Show($"You lost.\nThe right number is {randomNumber}");
                 UsernumberInput.Clear();
                 MinimumValueInput.Clear();
                 MaximumValueInput.Clear();
                 label.Content = "Now please enter number between minValue and maxValue";
-                count = 5;
+                ChancesInput.Clear();
                 return;
             }
 
@@ -113,44 +146,44 @@ namespace Number_Guessing_Game
 
             if (userNumber < randomNumber)
             {
-                if (count == 1)
+                if (chancesCount == 1)
                 {
-                    MessageBox.Show($"Wrong desicion.\nYou have {count} chance.\nPlease enter higher number");
+                    MessageBox.Show($"Wrong desicion.\nYou have {chancesCount-1} chance.\nPlease enter higher number");
 
                 }
                 else
                 {
-                    MessageBox.Show($"Wrong desicion.\nYou have {count} chances.\nPlease enter higher number");
+                    MessageBox.Show($"Wrong desicion.\nYou have {chancesCount-1} chances.\nPlease enter higher number");
 
                 }
                 UsernumberInput.Clear();
-                count--;
+                chancesCount--;
 
             }
             else if (userNumber > randomNumber)
             {
-                if (count == 1)
+                if (chancesCount == 1)
                 {
-                    MessageBox.Show($"Wrong desicion.\nYou have {count} chance.\nPlease enter lower number");
+                    MessageBox.Show($"Wrong desicion.\nYou have {chancesCount-1} chance.\nPlease enter lower number");
 
                 }
                 else
                 {
-                    MessageBox.Show($"Wrong desicion.\nYou have {count} chances.\nPlease enter lower number");
+                    MessageBox.Show($"Wrong desicion.\nYou have {chancesCount-1} chances.\nPlease enter lower number");
 
                 }
                 UsernumberInput.Clear();
-                count--;
+                chancesCount--;
 
             }
             else
             {
-                MessageBox.Show($"Congratulations.\nYou found right number at {6 - count}th time");
+                MessageBox.Show("Congratulations.\nYou found right number");
                 UsernumberInput.Clear();
                 MinimumValueInput.Clear();
                 MaximumValueInput.Clear();
                 label.Content = "Now please enter number between minValue and maxValue";
-                count = 5;
+                ChancesInput.Clear();
 
 
             }
